2fb31632859f1d8115dfa1722c58f5836662fb2e
Ross Turk <ross.turk@astronomer.io>
2022-07-10 23:01:06 -0400

    restore path mapper in lint function
    Signed-off-by: Ross Turk <ross.turk@astronomer.io>

--- site.sh
+++ site.sh
@@ -18,9 +18,8 @@
 # under the License.
 set -euo pipefail
 
-
+WORKING_DIR="$(pwd)"
 MY_DIR="$(cd "$(dirname "$0")" && pwd)"
-echo "MY_DIR: ${MY_DIR}"
 
 function log {
     echo -e "$(date +'%Y-%m-%d %H:%M:%S'):INFO: ${*} " >&2;
@@ -67,19 +66,63 @@ function ensure_that_website_is_build {
 }
 
 function run_command {
-    log "Running command: $*"
     working_directory=$1
     shift
-    pushd "${working_directory}"
+    log "Changing to ${working_directory} and running: $*@"
+    pushd "${working_directory}" &>/dev/null || exit 1
     "$@"
-    popd
+    popd &>/dev/null || exit 1
+}
+
+function relativepath {
+    source=$1
+    target=$2
+
+    common_part=$source # for now
+    result="" # for now
+
+    while [[ "${target#"$common_part"}" == "${target}" ]]; do
+        # no match, means that candidate common part is not correct
+        # go up one level (reduce common part)
+        common_part="$(dirname "$common_part")"
+        # and record that we went back, with correct / handling
+        if [[ -z $result ]]; then
+            result=".."
+        else
+            result="../$result"
+        fi
+    done
+
+    if [[ $common_part == "/" ]]; then
+        # special case for root (no common path)
+        result="$result/"
+    fi
+
+    # since we now have identified the common part,
+    # compute the non-common part
+    forward_part="${target#"$common_part"}"
+
+    # and now stick all parts together
+    if [[ -n $result ]] && [[ -n $forward_part ]]; then
+        result="$result$forward_part"
+    elif [[ -n $forward_part ]]; then
+        # extra slash removal
+        result="${forward_part:1}"
+    fi
+    echo "$result"
 }
 
 function run_lint {
     script_working_directory=$1
     command=$2
     shift 2
-    run_command "${script_working_directory}" "${command}" "${@}"
+
+    MAPPED_PATHS=()
+    for E in "${@}"; do
+        ABS_PATH=$(cd "${WORKING_DIR}" && realpath "${E}")
+        MAPPED_PATHS+=("${MY_DIR}/$(relativepath "$(pwd)" "${ABS_PATH}")")
+    done
+    run_command "${script_working_directory}" "${command}" "${MAPPED_PATHS[@]}"
 }
 
 function prepare_packages_metadata {
